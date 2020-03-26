a735f7545c93163cc2ba83ce5cbdcecfffb58bce
Didoshotev <didoshotev@gmail.com>
2020-03-26 10:48:05 +0200

    string_manipulation

--- string_manipulator.py
+++ string_manipulator.py
@@ -1,54 +1,34 @@
 text = input()
 
-def change(string, char, replacement):
-    string = string.repalce(char, replacement)
-    return string
-
-
-def includes(string, txt):
-    if txt in string:
-        return True
-    else:
-        return False
-
-
-def end(string, end_string):
-    idx = len(string) - len(end_string)
-    if string[idx:] == end_string:
-        return True
-    else:
-        return False
-
-
-def uppercase(string):
-    string = string.upper()
-    return string
-
-
-def find_index(string, char):
-    idx = string.index(char)
-    return idx
-
-
-def cut(string, start_index, length):
-    end_index = start_index + length
-    string = string[start_index:end_index]
-    return text
-
-
 while True:
     command = input().split(" ")
     if command[0] == "Done":
         break
     elif command[0] == "Change":
-        print(change(text, command[1], command[2]))
+        text = text.replace(command[1], command[2])
+        print(text)
     elif command[0] == "Includes":
-        print(includes(text, command[1]))
+        if command[1] in text:
+            print(True)
+        else:
+            print(False)
     elif command[0] == "End":
-        print(end(text, command[1]))
+        end = command[1]
+        idx = len(text) - len(end)
+        if text[idx:] == end:
+            print(True)
+        else:
+            print(False)
     elif command[0] == "Uppercase":
-        print(uppercase(text))
+        text = text.upper()
+        print(text)
     elif command[0] == "FindIndex":
-        print(find_index(text, command[1]))
+        char = command[1]
+        idx = text.index(char)
+        print(idx)
     elif command[0] == "Cut":
-        print(cut(text, command[1], command[2]))
+        start_index = int(command[1])
+        length = int(command[2])
+        end_index = start_index + length
+        text = text[start_index:end_index]
+        print(text)
